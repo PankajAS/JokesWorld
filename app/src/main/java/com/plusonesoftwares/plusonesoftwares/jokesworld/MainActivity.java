2b52bed4d5ef865d0dd60166b3fdc7ed086d86a6
Plus 3 <pankajs@plusonesoftwares.com>
2017-02-27 11:12:57 +0530

    Add Spinner

--- app/src/main/java/com/plusonesoftwares/plusonesoftwares/jokesworld/MainActivity.java
+++ app/src/main/java/com/plusonesoftwares/plusonesoftwares/jokesworld/MainActivity.java
@@ -1,13 +1,31 @@
 package com.plusonesoftwares.plusonesoftwares.jokesworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner lang, category;
+    ArrayList<String> string;
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lang  = (Spinner)findViewById(R.id.lang);
+        category = (Spinner)findViewById(R.id.category);
+
+        string = new ArrayList<>();
+        string.add("English");
+        string.add("Hindi");
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, string);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lang.setAdapter(adapter);
+
     }
 }
