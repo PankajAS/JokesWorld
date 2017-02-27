f328c4b142d6fe80b3fe8e79651a7957dc26b411
Plus 3 <pankajs@plusonesoftwares.com>
2017-02-27 13:22:48 +0530

    Add Share Mathod

--- app/src/main/java/com/plusonesoftwares/plusonesoftwares/jokesworld/DetailsViewActivity.java
+++ app/src/main/java/com/plusonesoftwares/plusonesoftwares/jokesworld/DetailsViewActivity.java
@@ -1,19 +1,11 @@
 package com.plusonesoftwares.plusonesoftwares.jokesworld;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.PopupWindow;
-import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by ashoksharma on 27/02/17.
@@ -23,23 +15,36 @@ public class DetailsViewActivity extends Activity {
     Button Close;
     Button Create;
     Button SMS;
+    Button FB;
+    TextView selectedItem;
+    String jokesDetails;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details_view);
         Create = (Button) findViewById(R.id.btnShare);
+        selectedItem = (TextView)findViewById(R.id.selected_Item);
+        Intent intent = getIntent();
+        jokesDetails = intent.getStringExtra("Name");
+        selectedItem.setText(jokesDetails);
 
         Create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             // TODO Auto-generated method stub
-                showPopup();
+                //showPopup();
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, jokesDetails);
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent, "Choose sharing method"));
+
             }
         });
     }
 
-    private PopupWindow pw;
+   /* private PopupWindow pw;
     private void showPopup() {
         try {
             // We need to get the instance of the LayoutInflater
@@ -56,6 +61,10 @@ public class DetailsViewActivity extends Activity {
 
             SMS = (Button) layout.findViewById(R.id.btnShareOnSMS);
             SMS.setOnClickListener(openSMS);
+            FB = (Button) layout.findViewById(R.id.btnShareOnFB);
+            FB.setOnClickListener(PostOnFB);
+
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -72,8 +81,17 @@ public class DetailsViewActivity extends Activity {
         public void onClick(View v) {
             Intent sendIntent = new Intent(Intent.ACTION_VIEW);
             sendIntent.setData(Uri.parse("sms:"));
-            sendIntent.putExtra("sms_body", "Jokes");
+            sendIntent.putExtra("sms_body", intentText);
             startActivity(sendIntent);
         }
     };
+    private View.OnClickListener PostOnFB = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+            shareIntent.setType("text/plain");
+            startActivity(Intent.createChooser(shareIntent, "Choose sharing method"));
+        }
+    };*/
 }
