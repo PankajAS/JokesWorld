fa1a0771b1905c57e7bf1277fc9fdb15e40c2a02
ashoksharma <User@123456>
2017-02-27 12:40:06 +0530

    Details view and popup window for share jokes.

---
+++ app/src/main/java/com/plusonesoftwares/plusonesoftwares/jokesworld/DetailsViewActivity.java
@@ -0,0 +1,63 @@
+package com.plusonesoftwares.plusonesoftwares.jokesworld;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.Spinner;
+
+/**
+ * Created by ashoksharma on 27/02/17.
+ */
+
+public class DetailsViewActivity extends Activity {
+    Button Close;
+    Button Create;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_view);
+        Create = (Button) findViewById(R.id.btnShare);
+
+        Create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            // TODO Auto-generated method stub
+                showPopup();
+            }
+        });
+    }
+
+    private PopupWindow pw;
+    private void showPopup() {
+        try {
+            // We need to get the instance of the LayoutInflater
+            LayoutInflater inflater = (LayoutInflater)DetailsViewActivity.this.getSystemService
+                    (Context.LAYOUT_INFLATER_SERVICE);
+
+            View layout = inflater.inflate(R.layout.popup,
+                    (ViewGroup) findViewById(R.id.popup_1));
+            pw = new PopupWindow(layout, ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+            pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+            Close = (Button) layout.findViewById(R.id.close_popup);
+            Close.setOnClickListener(cancel_button);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private View.OnClickListener cancel_button = new View.OnClickListener() {
+        public void onClick(View v) {
+            pw.dismiss();
+        }
+    };
+}
